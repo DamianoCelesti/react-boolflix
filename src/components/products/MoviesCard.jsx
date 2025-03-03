047c4fdf344936007e8afb1eb1d9bda801123210
DamianoCelesti <damiano.celesti@gmail.com>
2025-03-03 03:03:53 +0100

    milestone 2

--- src/components/products/MoviesCard.jsx
+++ src/components/products/MoviesCard.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import ReactCountryFlag from "react-country-flag";
+
+
 const MoviesCard = (props) => {
 
+
+    function bandiere(lingua) {
+        const bandiereNazioni = {
+            en: "GB",
+            it: "IT",
+            fr: "FR",
+            us: "US",
+        };
+
+        return bandiereNazioni[lingua] || "no flag";
+    }
     const { movie } = props
 
     return (
@@ -8,7 +23,11 @@ const MoviesCard = (props) => {
             <div key={movie.id}>
                 <h3>{movie.title}</h3>
                 <p>{movie.original_title}</p>
-                <p>{movie.original_language}</p>
+                <ReactCountryFlag
+                    countryCode={bandiere(movie.original_language)}
+                    svg
+                    title={movie.original_language}
+                />
                 <p>{movie.vote_average}</p>
 
             </div>
